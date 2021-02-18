7dc0a277f9748927a0ca328a3638010be9671caf
zmdemon <dmitriy.gaev66@gmail.com>
2021-02-18 10:37:03 +0300

    upd

--- 36_supreme/js/script.js
+++ 36_supreme/js/script.js
@@ -1,23 +1,10 @@
-const numbers1 = [4, 11, -5, 17, -10, 8, 56, 0];
+window.addEventListener("load", function () {
+    const dateSpan = document.querySelector(".date-span");
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
 
-const numbers2 = [{
-    id: 1, number: 10
-}, {
-    id: 2, number: -6
-}, {
-    id: 3, number: 22
-}];
+    dateSpan.innerHTML = mm + '/' + dd + '/' + yyyy;
 
-const lessThanTen = numbers1.filter(number => number < 10);
-const moreThanTen2 = numbers2.filter(numer => numer.number > 10);
-const moreThanZero = numbers2.filter(onlyPositive);
-
-function onlyPositive(array) {
-        return (array.number !== 0);
-
-}
-
-
-console.log(lessThanTen);
-console.log(moreThanTen2[0]);
-console.log(moreThanZero);
+})
